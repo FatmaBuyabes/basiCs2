e7c877412c72493dcc69317b6529e1cdb5cbf1df
buyab_h9pfnco <buyab_h9pfnco@MOSHOPC>
2024-04-18 23:02:40 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+      int currentHour = 10;
+
+
+      if (currentHour >= 11 || currentHour <= 14)
+      {
+            Console.WriteLine("Time for lunch!");
+}
+
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+}
+
+
+//bonus
+
+        int x = 5;
+
+        while (x > 0)
+        {
+            Console.WriteLine(x);
+            x--;
+}
+
+
+        string[] daysOfWeek = { "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday" };
+
+       
+        foreach (string day in daysOfWeek)
+        {
+            Console.WriteLine(day);
+}
+
+        int[] numbers = { 1, 2, 3, 4, 5 , 6,7};
+
+        int sum = 0;
+
+        
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        
+        Console.WriteLine("The sum of the numbers is: " + sum);
+
+
